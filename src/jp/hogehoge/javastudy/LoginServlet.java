3ba74c7b037ed7e76e0d331b12cffb3849fa8f93
Yuta <y.sugiyama54@gmail.com>
2020-12-24 20:16:39 +0900

    コメントアウトを除去

--- src/jp/hogehoge/javastudy/LoginServlet.java
+++ src/jp/hogehoge/javastudy/LoginServlet.java
@@ -19,34 +19,34 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-//		// 変数の初期設定
-//		String name = "ゲスト"; // 名前の初期値設定
-//		String status = "ログイン失敗"; // ログインステータス
-//
-//		// 文字コードの初期設定
-//		request.setCharacterEncoding("Windows-31J");
-//
-//		// IDの取得
-//		String id = request.getParameter("id");
-//
-//		// パスワードの取得
-//		String pass = request.getParameter("pass");
-//
-//		// IDとパスワードのチェック
-//		if (id.equals("search") && pass.equals("man")) {
-//			status = "ログイン成功";
-//			name = "佐藤";
-//		}
-//
-//		// リクエストにデータを追加する
-//		request.setAttribute("login", status);
-//
-//		// HttpSessionの作成とセッションにデータを追加する
-//		HttpSession session = request.getSession( true );
-//		session.setAttribute( "name", name );
-//
-//		// result.jspへ転送
-//		request.getRequestDispatcher("/result.jsp").forward(request, response);
+		// 変数の初期設定
+		String name = "ゲスト"; // 名前の初期値設定
+		String status = "ログイン失敗"; // ログインステータス
+
+		// 文字コードの初期設定
+		request.setCharacterEncoding("Windows-31J");
+
+		// IDの取得
+		String id = request.getParameter("id");
+
+		// パスワードの取得
+		String pass = request.getParameter("pass");
+
+		// IDとパスワードのチェック
+		if (id.equals("search") && pass.equals("man")) {
+			status = "ログイン成功";
+			name = "佐藤";
+		}
+
+		// リクエストにデータを追加する
+		request.setAttribute("login", status);
+
+		// HttpSessionの作成とセッションにデータを追加する
+		HttpSession session = request.getSession( true );
+		session.setAttribute( "name", name );
+
+		// result.jspへ転送
+		request.getRequestDispatcher("/result.jsp").forward(request, response);
 
 	}
 }
